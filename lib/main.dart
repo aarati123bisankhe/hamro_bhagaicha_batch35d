9cb470ccac849dd7b6ef12cf4f4ee5139420c0a2
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-23 15:18:15 +0545

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,3 @@
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:hamro_bhagaicha_batch35d/app/app.dart';
-// import 'package:hamro_bhagaicha_batch35d/core/services/hive/hive_service.dart';
-// import 'package:hamro_bhagaicha_batch35d/core/services/storage/user_session_service.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // Initialize Hive
-//   final hiveService = HiveService();
-//   await hiveService.init();
-
-//   // Initialize SharedPreferences
-//   final prefs = await SharedPreferences.getInstance();
-
-//   // Create ProviderContainer with overrides
-//   final container = ProviderContainer(
-//     overrides: [
-//       hiveServiceProvider.overrideWithValue(hiveService),
-//       sharedPreferencesProvider.overrideWithValue(prefs),
-//     ],
-//   );
-
-//   runApp(
-//     UncontrolledProviderScope(
-//       container: container,
-//       child: const MyApp(),
-//     ),
-//   );
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -41,31 +6,6 @@ import 'package:hamro_bhagaicha_batch35d/core/services/hive/hive_service.dart';
 import 'package:hamro_bhagaicha_batch35d/core/services/storage/user_session_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // Initialize Hive
-//   final hiveService = HiveService();
-//   await hiveService.init();
-
-//   // Initialize SharedPreferences
-//   final prefs = await SharedPreferences.getInstance();
-
-//   // Create ProviderContainer with overrides
-//   final container = ProviderContainer(
-//     overrides: [
-//       hiveServiceProvider.overrideWithValue(hiveService),
-//       sharedPreferencesProvider.overrideWithValue(prefs),
-//     ],
-//   );
-
-//   runApp(
-//     UncontrolledProviderScope(
-//       container: container,
-//       child: const MyApp(),
-//     ),
-//   );
-// }
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
