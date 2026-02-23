9c5c362a78c19f46e23c7e970bdd8470016d5651
Aarati Thapa <aaratithapa@Aaratis-MacBook-Air.local>
2026-02-23 15:15:51 +0545

    complete update profile

--- lib/features/auth/domain/usecase/update_profile_image_usecase.dart
+++ lib/features/auth/domain/usecase/update_profile_image_usecase.dart
@@ -1,28 +1,3 @@
-// import 'dart:io';
-// import 'package:dartz/dartz.dart';
-// import 'package:hamro_bhagaicha_batch35d/core/error/failure.dart';
-// import 'package:hamro_bhagaicha_batch35d/features/auth/data/repositories/auth_repository.dart';
-// import 'package:hamro_bhagaicha_batch35d/features/auth/domain/entities/auth_entity.dart';
-
-// //provider
-// final updateProfileImageUseCaseProvider = UpdateProfileImageUseCaseProvider();
-
-// class UpdateProfileImageUseCaseProvider {
-//   UpdateProfileImageUseCase call(AuthRepository repository) {
-//     return UpdateProfileImageUseCase(repository);
-//   }
-// }
-
-// class UpdateProfileImageUseCase {
-//   final AuthRepository repository;
-
-//   UpdateProfileImageUseCase(this.repository);
-
-//   Future<Either<Failure, AuthEntity>> call(File imageFile) {
-//     return repository.updateProfileImage(imageFile);
-//   }
-// }
-
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dartz/dartz.dart';
